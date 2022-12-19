48aaa7abad854c73ac15654de02151d397c461a8
Ankit Rai <raia85859@gmail.com>
2022-12-19 19:40:26 +0545

    project intilize

---
+++ vite-project/src/pages/Home.jsx
@@ -0,0 +1,64 @@
+import { useEffect, useState } from "react"
+import axios from "axios"
+import { responsivePropType } from "react-bootstrap/esm/createUtilityClasses"
+import { MovieCard } from "../components/MovieCard"
+export const Home = () => {
+    let [nowPlaying, setNowPlaying] = useState([])
+    let [topRated, setTopRated] = useState([])
+    useEffect(() => {
+        axios.get(`${import.meta.env.VITE_API_URL}movie/now_playing?api_key=${import.meta.env.VITE_API_KEY}`).then(
+            (response) => {
+                setNowPlaying(response.data.results);
+            }
+        ).catch(
+            (err) => {
+
+            }
+        )
+
+        axios.get(`${import.meta.env.VITE_API_URL}movie/top_rated?api_key=${import.meta.env.VITE_API_KEY}`).then(
+            (response) => {
+                setTopRated(response.data.results);
+            }
+        ).catch(
+            (err) => {
+
+            }
+        )
+
+    }, []
+    )
+
+    return <>
+        <main className="row">
+            <div className="col-12">
+                <div className="row">
+                    <div className="col-12">
+                        <h3> In Theaters</h3>
+                    </div>
+                </div>
+                <div className="row row-cols-5">
+                    {
+                        nowPlaying.slice(0, 5).map(
+                            (movie) => <MovieCard movie={movie} />
+                        )
+                    }
+                </div>
+            </div>
+            <div className="col-12">
+                <div className="row">
+                    <div className="col-12">
+                        <h3> Top Rated</h3>
+                    </div>
+                </div>
+                <div className="row row-cols-5">
+                    {
+                        topRated.slice(0, 5).map(
+                            (movie) => <MovieCard movie={movie} />
+                        )
+                    }
+                </div>
+            </div>
+        </main>
+    </>
+}   
---
+++ vite-project/src/siteRoutes.jsx
@@ -0,0 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { Home } from "./pages/Home"
+import { Layout } from "./pages/Layout"
+
+
+export const SiteRoutes = () => {
+
+    return <>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Layout />}>
+                    <Route index element={<Home />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    </>
+}
